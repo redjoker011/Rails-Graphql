c9ccce3b8d9cdf765fde587186d52b52902b2a42
Peter John Alvarado <redjoker011@gmail.com>
2018-04-12 16:58:55 +0800

    Handle Validation Errors

--- app/graphql/graphql_app_schema.rb
+++ app/graphql/graphql_app_schema.rb
@@ -2,3 +2,9 @@ GraphqlAppSchema = GraphQL::Schema.define do
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
+GraphQL::Errors.configure(GraphqlAppSchema) do
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    errors = exception.record.errors.full_messages
+    GraphQL::ExecutionError.new(errors.join("\n"))
+  end
+end
